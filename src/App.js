e2131a2ce9d1bb91c0fc278aa2208a88b5a7bad7
GeorgiSap <georgisapundzhiev@hotmail.com>
2018-07-13 03:33:02 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 import Shelf from './Shelf';
+import * as BooksAPI from './BooksAPI';
+import { Link } from 'react-router-dom'; 
+import { Route } from 'react-router-dom';
 
 class App extends Component {
+
+  state = {
+    books : []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({books : books }))  
+  }
+
   render() {
     return (
-      <Shelf/>
+      <div>
+        <Route exact path="/" render={(() => (
+          <div>
+            <Shelf category="Currently Reading" books={this.state.books} />
+            <Shelf category="Want to Read" books={this.state.books} />
+            <Shelf category="Read" books={this.state.books} />
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
+            </div>
+        </div>
+        ))}/>
+        <Route path="/search" render={(() => (
+            <div>
+              <Shelf category="None" books={this.state.books} />
+            </div>
+        ))}/>
+      </div>
     );
   }
 }
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import Book from './Book';
-import * as BooksAPI from './BooksAPI';
 
 class Shelf extends Component {
-
-	state = {
-		books : []
-	}
-
-	componentDidMount() {
-		BooksAPI.getAll().then(books => this.setState({books : books }))	
-	}
 
 	render() {
 		return (
 			<div className="bookshelf">
-				<h2 className="bookshelf-title">Currently Reading</h2>
+				<h2 className="bookshelf-title">{this.props.category}</h2>
 				<div className="bookshelf-books">
 					<ol className="books-grid">
-						{this.state.books.map(book => (
+						{this.props.books.map(book => (
 						<li key={book.id}>
 							<Book book={book} />
 						</li>
